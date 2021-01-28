1ee3719be7df6dcdfd52666245ea8fb36d4854d5
run-after <run.after@protonmail.com>
2021-01-28 09:06:03 -0600

    Modify background for character images and insert them into header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import '../styles/Header.css';
+import waldo from '../media/waldo.png';
+import wanda from '../media/wanda.png';
+import odlaw from '../media/odlaw.png';
+import wizard from '../media/wizard.png';
+
 
 const Header = () => {
 
   return (
     <div className='header'>
       <div className='characters'>
-        <div className='placeholder'></div>
-        <div className='placeholder'></div>
-        <div className='placeholder'></div>
+        <img src={waldo} alt='waldo'/>
+        <img src={wanda} alt='wanda'/>
+        <img src={odlaw} alt='odlaw'/>
+        <img src={wizard} alt='wizard'/>
       </div>
       <div className='timer'>00:00:00</div>
     </div>
