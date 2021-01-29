e357d4f3867a7f02049bb417621bb88c665a924c
run-after <run.after@protonmail.com>
2021-01-29 14:32:03 -0600

    Style person selector dropdown

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -4,28 +4,41 @@ import { useEffect } from 'react';
 
 const Picture = () => {
   // will proabaly put this into state
-  let characters = ['waldo', 'wanda', 'odlaw', 'wizz'];
+  let characters = ['waldo', 'wanda', 'odlaw', 'wizard'];
 
   const popUpSelection = (coords) => {
-    const div = document.createElement('div');
-    div.classList.add('selector');    
-    div.style = `left: ${coords.x}px; top: ${coords.y}px;`;
+    const selector = document.createElement('div');
+    selector.classList.add('selector');    
+    selector.style = `left: ${coords.x}px; top: ${coords.y}px;`;
     const pic = document.querySelector('.picture');
-    pic.appendChild(div);
+    pic.appendChild(selector);
   };
+
+  const dropDownSelection = (coords) => {
+    const menu = document.createElement('div');
+    menu.classList.add('menu');
+    menu.style = `left: ${coords.x}px; top: ${coords.y}px;`;
+    characters.forEach((person) => {
+      const btn = document.createElement('button');
+      btn.classList.add('person-selector');
+      btn.textContent = person;
+      btn.value = person;
+      menu.appendChild(btn);
+    })
+    const pic = document.querySelector('.picture');
+    pic.appendChild(menu);
+  };
+
 
   useEffect(() => {
     const img = document.getElementById('image');
     
     img.addEventListener('click', (e) => {
-      popUpSelection({x: (e.layerX - 25), y: (e.layerY - 25)});
+      popUpSelection({ x: (e.layerX - 25), y: (e.layerY - 25) });
+      dropDownSelection( {x: e.layerX + 25, y: e.layerY + 25})
     })
 
   }, []);
-
-  
-
-  
 
   return (
     <div className='picture'>
