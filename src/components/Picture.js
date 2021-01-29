930bccc65435c992a3be8ac93491bc8793720cbf
run-after <run.after@protonmail.com>
2021-01-29 10:37:45 -0600

    Add popup selector functionality

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,11 +1,35 @@
 import image from '../media/maps_troy.jpeg';
 import '../styles/Picture.css';
+import { useEffect } from 'react';
 
 const Picture = () => {
+  // will proabaly put this into state
+  let characters = ['waldo', 'wanda', 'odlaw', 'wizz'];
+
+  const popUpSelection = (coords) => {
+    const div = document.createElement('div');
+    div.classList.add('selector');    
+    div.style = `left: ${coords.x}px; top: ${coords.y}px;`;
+    const pic = document.querySelector('.picture');
+    pic.appendChild(div);
+  };
+
+  useEffect(() => {
+    const img = document.getElementById('image');
+    
+    img.addEventListener('click', (e) => {
+      popUpSelection({x: (e.layerX - 25), y: (e.layerY - 25)});
+    })
+
+  }, []);
+
+  
+
+  
 
   return (
     <div className='picture'>
-      <img src={image} />
+      <img id='image' src={image} alt='Troy' />
     </div>
   );
 };
