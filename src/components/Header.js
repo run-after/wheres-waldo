cd5d6a9884521a1604c23f0337de503dae685203
run-after <run.after@protonmail.com>
2021-01-30 11:02:48 -0600

    Load character images from Firebase

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,49 @@
 import '../styles/Header.css';
-import waldo from '../media/waldo.png';
-import wanda from '../media/wanda.png';
-import odlaw from '../media/odlaw.png';
-import wizard from '../media/wizard.png';
+import firebase from 'firebase';
 
 
 const Header = () => {
+
+  const storage = firebase.storage();
+  const storageRef = storage.ref();
+
+  storageRef.child('waldo.png').getDownloadURL().then((url) => {
+    const waldo = document.querySelector('#waldo-img');
+    waldo.setAttribute('src', url);
+  }).catch((error) => {
+    console.log(error)
+  });
+
+  storageRef.child('wanda.png').getDownloadURL().then((url) => {
+    const waldo = document.querySelector('#wanda-img');
+    waldo.setAttribute('src', url);
+  }).catch((error) => {
+    console.log(error)
+  });
+
+  storageRef.child('odlaw.png').getDownloadURL().then((url) => {
+    const waldo = document.querySelector('#odlaw-img');
+    waldo.setAttribute('src', url);
+  }).catch((error) => {
+    console.log(error)
+  });
+
+  storageRef.child('wizard.png').getDownloadURL().then((url) => {
+    const waldo = document.querySelector('#wizard-img');
+    waldo.setAttribute('src', url);
+  }).catch((error) => {
+    console.log(error)
+  });
 
   return (
     <div className='header'>
       <div className='character-container'>
         <h6>Characters</h6>
         <div className='characters'>
-          <img id='waldo-img' src={waldo} alt='waldo'/>
-          <img id='wanda-img' src={wanda} alt='wanda'/>
-          <img id='odlaw-img' src={odlaw} alt='odlaw'/>
-          <img id='wizard-img' src={wizard} alt='wizard'/>
+          <img id='waldo-img' alt='waldo'/>
+          <img id='wanda-img' alt='wanda'/>
+          <img id='odlaw-img' alt='odlaw'/>
+          <img id='wizard-img' alt='wizard'/>
         </div>
         
       </div>
