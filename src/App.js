59fe7e75f23ea991db194401b932f5ebc0f1ce2f
run-after <run.after@protonmail.com>
2021-02-04 14:26:30 -0600

    Log in anonymously on mount

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './styles/App.css';
 import Picture from './components/Picture';
 import Header from './components/Header';
+import firebase from 'firebase';
+import { useEffect } from 'react';
 
 function App() {
+  // Sign in anonymously
+  useEffect(() => {
+    firebase.auth().signInAnonymously().then(() => {
+      console.log('signedin as', firebase.auth().currentUser.uid);
+    }).catch((error) => {
+      console.log(error)
+    });    
+  },[]);
+
   return (
     <div className='container'>
       <Header />
@@ -12,3 +23,7 @@ function App() {
 };
 
 export default App;
+
+// signing in ANON
+// write startTime to DB
+// when finished, write endTime to DB
