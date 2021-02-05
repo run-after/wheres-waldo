6f0ba705ad329d4cb186dc4d89a3a3c2f3b7a27b
run-after <run.after@protonmail.com>
2021-02-05 15:09:49 -0600

    Correct name from wanda to wilma

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import Timer from './Timer';
 import '../styles/Header.css';
 import firebase from 'firebase';
+import 'firebase/storage';// not sure if doing anything
 
 
 const Header = () => {
@@ -17,8 +18,8 @@ const Header = () => {
     console.log(error)
   });
 
-  storageRef.child('wanda.png').getDownloadURL().then((url) => {
-    const waldo = document.querySelector('#wanda-img');
+  storageRef.child('wilma.png').getDownloadURL().then((url) => {
+    const waldo = document.querySelector('#wilma-img');
     waldo.setAttribute('src', url);
   }).catch((error) => {
     console.log(error)
@@ -38,19 +39,16 @@ const Header = () => {
     console.log(error)
   });
 
-
-
   return (
     <div className='header'>
       <div className='character-container'>
         <h6>Characters</h6>
         <div className='characters'>
           <img id='waldo-img' alt='waldo'/>
-          <img id='wanda-img' alt='wanda'/>
+          <img id='wilma-img' alt='wilma'/>
           <img id='odlaw-img' alt='odlaw'/>
           <img id='wizard-img' alt='wizard'/>
         </div>
-        
       </div>
       <Timer />
     </div>
