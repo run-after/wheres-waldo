db7b374ca7304fe9b2c6e53878a38383cf5176ae
run-after <run.after@protonmail.com>
2021-02-10 14:03:38 -0600

    Remove unneeded function to format timer

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -107,27 +107,6 @@ const Picture = (props) => {
       console.log('Wrong');
     };
   };
-  
-  // Maybe can move this to a different module... this and from Timer
-  const formatTimer = (time) => {
-    let min = Math.floor(time / 60000);
-    if (min > 59) {
-      min = min % 60;
-    };
-    let sec = Math.floor(time / 1000);
-    if (sec > 59) {
-      sec = sec % 60;
-    };
-    let centiSec = Math.floor(time / 100);
-    if (centiSec > 10) {
-      centiSec = centiSec % 10;
-    };
-    let milSec = Math.floor(time / 10);
-    if (milSec > 10) {
-      milSec = milSec % 10;
-    };
-    return `${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}:${centiSec}${milSec}`;
-  };
 
   useEffect(() => {
     const img = document.getElementById('image');
